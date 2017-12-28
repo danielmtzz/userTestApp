99b81bb195da9ff6863cb57f2b7fd8deeced51d3
danielmtzz <34920524+danielmtzz@users.noreply.github.com>
2017-12-28 11:24:08 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
  module.exports = {
  'port': process.env.PORT || 8080,
- 'database': 'mongodb://localhost/test',
+ 'database': 'mongodb://danielmtzz:123456@ds133597.mlab.com:33597/test1',
  'secret': 'ilovescotchscotchyscotchscotch'
  };
